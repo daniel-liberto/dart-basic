6601c060029787d608730a24c9854e5d48822538
daniel-liberto <daniel.liberto@hotmail.com>
2023-06-07 18:40:35 -0300

    shortening variables

--- constructor.dart
+++ constructor.dart
@@ -11,17 +11,12 @@ class User {
   String? person = '';
 
   // constructor method, where "this" is self reference
-  User(name, age, phone) {
-    this.name = name;
-    this.age = age;
-    this.phone = phone;
-  }
+  User(this.name, this.age, this.phone);
 
   // just a function inside the class
   void hello(person) {
-    print('Hello $person');
+    print('\nHello $person');
     print(
-        'My name is ${this.name} and my age is ${this.age.toString()} years old');
-    print('If you need my contact, my phone is ${this.phone}');
+        '\nMy name is ${this.name} and my age is ${this.age.toString()} years old \nIf you need my contact, my phone is ${this.phone}\n');
   }
 }
