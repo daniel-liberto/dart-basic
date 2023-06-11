73e0dda969312d965fe340202510a05fd4319a84
daniel-liberto <daniel.liberto@hotmail.com>
2023-06-11 10:03:34 -0300

    minor changes

--- const-final.dart
+++ const-final.dart
@@ -1,10 +1,12 @@
 void main() {
   // const is when you know the entire life cicle of the variable.
   const int a = 2;
+  print(a);
 
   // final have the almost same "lock" system, but is recommended for use
   // when you don't know the initial value.
   final int b = 2;
+  print(b);
 
   final DateTime data = new DateTime.now();
 
